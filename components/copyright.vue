97cb36fb31ebf62021118b7773a531f4cf216b3c
星月なるこ <projectcsj@gmail.com>
2025-04-11 02:12:22 +0800

    feat: add copyright component with dynamic year display

---
+++ components/copyright.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  year: number
+  name: string
+  url: string
+  long: boolean
+}>()
+const current = new Date().getFullYear()
+const years = computed(() =>
+  props.year === current ? `${current}` : `${props.year} - ${current}`,
+)
+</script>
+
+<template>
+  <div>
+    <span v-if="props.long">
+      Copyright &copy; {{ years }}
+      <a :href="props.url" target="_blank">{{ props.name }}</a
+      >. All rights reserved.
+    </span>
+    <span v-else>
+      &copy; {{ years }}
+      <a :href="props.url" target="_blank">{{ props.name }}</a>
+    </span>
+  </div>
+</template>
