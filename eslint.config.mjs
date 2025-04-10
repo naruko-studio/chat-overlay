d74a6c1bd03630c6a33e0715bfad8f1e959b8125
星月なるこ <projectcsj@gmail.com>
2025-04-11 01:10:37 +0800

    init: Project Setup

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs"
+
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+
+export default withNuxt(eslintPluginPrettier, {
+  rules: {
+    "vue/no-multiple-template-root": "off",
+    "vue/multi-word-component-names": "off",
+  },
+})
