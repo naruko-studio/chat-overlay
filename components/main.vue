ef029eec2fc9d1075d352185f4b6b62a2b353653
星月なるこ <projectcsj@gmail.com>
2025-04-11 22:26:13 +0800

    feat: create main component with flexible layout and slot support

---
+++ components/main.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    as?: string
+    class?: string
+  }>(),
+  {
+    as: "main",
+    class: "",
+  },
+)
+</script>
+
+<template>
+  <component
+    :is="props.as"
+    :class="[
+      'flex w-full flex-1 flex-col gap-8 py-8',
+      'px-4 sm:px-6',
+      'bg-(--ui-bg) text-(--ui-text)',
+      props.class,
+    ]"
+  >
+    <slot />
+  </component>
+</template>
